fb0e2fef5d1f2185bf88ab9cdfbb29f509a9de48
Michael McPherson <michael@mpmcpherson.com>
2020-10-22 12:21:22 -0400

    scraper

---
+++ vacuum.php
@@ -0,0 +1,48 @@
+<?php
+
+dateGen();
+
+
+
+function dateGen()
+{
+	//The date that you want to start at.
+	$start = '2001-12-19';
+	 
+	//The date that you want to stop at.
+	$end = '2020-12-31';
+	 
+	//We set our counter to the start date.
+	$currentDate = strtotime($start);
+	 
+	//While the current timestamp is smaller or equal to
+	//the timestamp of the end date.
+	while($currentDate <= strtotime($end)){
+	    
+	    //Format the timestamp and print it out
+	    //for illustrative purposes.
+	    $formatted = date("mdY", $currentDate);
+	    $urlString = "https://somethingpositive.net/wp-content/uploads/".date("Y", $currentDate) . "/" . date("m", $currentDate) . "/sp".$formatted;
+	    
+	    sleep(10);
+	    echo $urlString.".png\n";
+
+	    saveIt($urlString.".png",getcwd()."/comics/sp".date("Ymd",$formatted).".png");
+	    
+	    sleep(10);
+	    echo $urlString .".gif\n";
+	    saveIt($urlString .".gif",getcwd()."/comics/sp".date("Ymd",$formatted).".gif");
+	    
+	    //Add one day onto the timestamp / counter.
+	    $currentDate = strtotime("+1 day", $currentDate);
+	}
+}
+
+function saveIt($url,$target)
+{
+	file_put_contents($target, file_get_contents($url));
+
+}
+
+
+?>
